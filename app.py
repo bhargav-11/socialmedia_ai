7b4aa9df1c85f3af51ceedce90122de4bc6172c2
Bhargav <tavethiyabhargav@gmail.com>
2023-08-26 17:13:29 +0530

    Added api to generate post

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from facebook import post_to_facebook
 from linkedin import post_to_linkedin
 from twitter import post_to_twitter
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
+from dotenv import load_dotenv
+import os
+import openai
+
 app = Flask(__name__)
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 def create_payload(data):
     try:
@@ -48,7 +55,37 @@ def twitter():
     response =  post_to_twitter(payload)
     return {"message" : "Successfully posted"} 
 
+@app.route('/generate-post', methods=['POST'])
+def generate_post():
+    data = request.get_json()
 
+    command = data.get('command')
+    goal = data.get('goal')
+    tone = data.get('tone')
+
+    if not (command and goal and tone):
+        return jsonify({'error': 'Missing required parameters'}), 400
+
+    prompt = """
+    Write a 500 words social media post based on below data points.
+
+    command: {}
+    goal: {}
+    tone: {}
+    """.format(command, goal, tone)
+
+    response = openai.ChatCompletion.create(
+              model="gpt-4-0613",
+              messages=[{"role": "system", "content": 'You are a helpful assistant to create social media posts'},
+                        {"role": "user", "content": prompt}
+              ])
+    
+    post_content = response['choices'][0]['message']['content']
+
+    response = {
+        'generated_post': post_content
+    }
+    return jsonify(response)
     
 
 if __name__ == '__main__':
